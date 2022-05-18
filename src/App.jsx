663642866c8d935f1fcfdc178593df1e3c31d95c
Andres Arango <ararango@comcast.net>
2022-05-18 12:35:30 -0700

    build out app framework

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import PropTypes from 'prop-types';
+
+function App(props) {
+  const { name } = props;
+  return (
+    <>
+      <h1>
+        Hello
+        {` ${name}`}
+      </h1>
+      <div>Sup</div>
+    </>
+  );
+}
+
+App.propTypes = {
+  name: PropTypes.string,
+};
+
+App.defaultProps = {
+  name: 'Verde Customer',
+};
+
+export default hot(App);
