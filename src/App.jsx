7ea928f5af865723e0c168e7981d7d1fe944eafe
lucky89nana <lilinna0809@gmail.com>
2022-05-19 15:55:59 -0700

    skeleton for QnA

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import PropTypes from 'prop-types';
 import Overview from './components/overview/Overview';
+import QnA from './components/questions_answers/QnA';
 
 function App(props) {
   const { name } = props;
@@ -12,6 +13,7 @@ function App(props) {
         {` ${name}`}
       </h1>
       <Overview />
+      <QnA />
     </>
   );
 }
---
+++ src/components/questions_answers/QnA_Components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Search() {
+  return (
+    <from>
+      <input type="text" placeholder="Search for answers..." />
+    </from>
+  );
+}
