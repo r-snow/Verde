9d05063a50319f620bb9e3f19774a643beded8ce
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-05-19 14:21:05 -0700

    RelatedItems component stubbed.

---
+++ src/components/related_items_outfit/relatedItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RelatedItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // related products from API call
+    };
+  }
+
+  render() {
+    return <div>{/* product cards */}</div>;
+  }
+}
