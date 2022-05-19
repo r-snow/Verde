ffa4d67e1c1fd76ea9e73ef203eae81946ace742
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-05-19 15:19:16 -0700

    Stubbed out riProductCard, added a sample price file.

--- src/components/related_items_outfit/riProductCard.jsx
+++ src/components/related_items_outfit/riProductCard.jsx
@@ -1,1 +1,19 @@
 import React from 'react';
+// import Product from './sampleRelatedItems';
+import Price from './price';
+
+function ProductCard(props) {
+  return (
+    <div>
+      <ProductImage />
+      <p>Jackets</p>
+      {/* Product Name: */}
+      <p>Camo Onesie</p>
+      {/* Price: */}
+      <Price price={140} salePrice={120} />
+      {/* Star Rating */}
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/components/related_items_outfit/sampleRelatedItems.js
+++ src/components/related_items_outfit/sampleRelatedItems.js
@@ -129,3 +129,5 @@ const retlatedItemsv = [
     ],
   },
 ];
+
+export default product;
