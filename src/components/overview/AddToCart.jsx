bd48e7902a904b64cd1fa4ee23c78fab6edd3e0c
Andres Arango <ararango@comcast.net>
2022-05-19 15:47:36 -0700

    Create and implement skeleton SizeSelector component

---
+++ src/components/overview/AddToCart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SizeSelector from './SizeSelector';
+
+export default function AddToCart() {
+  return (
+    <div>
+      <span>
+        <SizeSelector />
+        {/* <QtySelector /> */}
+      </span>
+      {/* <AddToCartBtn /> */}
+    </div>
+  );
+}
