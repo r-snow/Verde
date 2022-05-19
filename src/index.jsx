c38a5c5aa436a964636c66b8ced18140e410cd61
Andres Arango <ararango@comcast.net>
2022-05-18 18:48:42 -0700

    update site title, change convention to match React v18

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const mountNode = document.getElementById('app');
-ReactDOM.render(<App name="Jane" />, mountNode);
+const root = createRoot(mountNode);
+root.render(<App name="Verde Customer" />);
