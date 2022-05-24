2e832a10fbd8b1b3ea75c504a72d32c9acc9fdb3
Kenny <Kennyytran95@gmail.com>
2022-05-23 17:03:02 -0700

    debug

---
+++ src/components/ratings_reviews/ReviewPhotos.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function ReviewPhotos({ photo }) {
+  const [popUpPhoto, openPicture] = useState(false);
+
+  const handleOpenPhoto = () => {
+    openPicture(!popUpPhoto);
+  };
+
+  return (
+    <div>
+      <img
+        src={photo.url}
+        key={photo.id}
+        alt="fashion-pictures"
+        className="review-thumbnails"
+        role="presentation"
+        onClick={handleOpenPhoto}
+        onKeyPress={handleOpenPhoto}
+      />
+      {popUpPhoto ? (
+        <div className="full-photo-modal-container">
+          <div
+            className="modal-overlay"
+            onClick={handleOpenPhoto}
+            role="button"
+            tabIndex={0}
+            onKeyDown={handleOpenPhoto}
+            aria-label="close modal with overlay"
+          />
+          <img
+            src={photo.url}
+            key={photo.id}
+            alt="fashion-pictures"
+            className="review-photo-modal"
+            role="presentation"
+            onClick={(event) => event.stopPropagation()}
+            onKeyPress={handleOpenPhoto}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+ReviewPhotos.propTypes = {
+  photo: PropTypes.string.isRequired,
+};
+
+export default ReviewPhotos;
