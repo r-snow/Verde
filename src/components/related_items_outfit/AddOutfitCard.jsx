f5eed07372dfcf54a3a329acd5cd664856e07ef7
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-05-25 16:37:23 -0700

    Added plus icon to add outift card.

--- src/components/related_items_outfit/AddOutfitCard.jsx
+++ src/components/related_items_outfit/AddOutfitCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function AddOutfitCard() {
   return (
-    <div>
-      <p>+</p>
+    <div className="add-outfit-card">
+      <FontAwesomeIcon icon={faCirclePlus} size="lg" type="button" />
       <p>Add Outfit</p>
     </div>
   );
