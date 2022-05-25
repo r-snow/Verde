495be5db0f49849b4a4e49a08d1f0efde072fa9c
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-05-24 17:02:30 -0700

    Started building out a feature to have the modal close when the user clicks anywhere outside of it.

--- src/components/related_items_outfit/CompareModal.jsx
+++ src/components/related_items_outfit/CompareModal.jsx
@@ -3,39 +3,49 @@ import PropTypes from 'prop-types';
 
 export default function Compare({ setOpenModal }) {
   return (
-    <div className="compare-modal">
-      <button
-        className="compare-modal-close"
-        type="button"
+    <div className="compare-modal-container">
+      <div
+        className="compare-overlay"
         onClick={() => setOpenModal(false)}
-      >
-        X
-      </button>
-      <h2>COMPARING</h2>
-      <table>
-        <tbody>
+        role="button"
+        tabIndex={0}
+        onKeyDown={() => setOpenModal(false)}
+        aria-label="close modal with overlay"
+      />
+      <div className="compare-modal">
+        <button
+          className="compare-modal-close"
+          type="button"
+          onClick={() => setOpenModal(false)}
+        >
+          x
+        </button>
+        <h2>COMPARING</h2>
+        <table className="table">
           <tr className="column-title">
             <th>Current Product Name</th>
             <th>Attribute</th>
             <th>Compare Product Name</th>
           </tr>
-          <tr className="table-check">
-            <td>&#10004;</td>
-            <td>&#10004;</td>
-            <td>&#10004;</td>
-          </tr>
-          <tr className="features">
-            <td>Ultrasheen</td>
-            <td>LightCompose</td>
-            <td>100% Cotton</td>
-          </tr>
-          <tr className="table-check">
-            <td>&#10004;</td>
-            <td>&#10004;</td>
-            <td>&#10004;</td>
-          </tr>
-        </tbody>
-      </table>
+          <tbody className="table-body">
+            <tr className="table-row">
+              <td>&#10004;</td>
+              <td>&#10004;</td>
+              <td>&#10004;</td>
+            </tr>
+            <tr className="table-row">
+              <td>Ultrasheen</td>
+              <td>Light Compose</td>
+              <td>100% Cotton</td>
+            </tr>
+            <tr className="table-row">
+              <td>&#10004;</td>
+              <td>&#10004;</td>
+              <td>&#10004;</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
