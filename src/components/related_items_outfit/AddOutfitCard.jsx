e0f1cfba39ad63c7a5cccda29564679d4d941172
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-05-25 18:06:32 -0700

    Building out yourOutfit skeleton.

--- src/components/related_items_outfit/AddOutfitCard.jsx
+++ src/components/related_items_outfit/AddOutfitCard.jsx
@@ -4,8 +4,19 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function AddOutfitCard() {
   return (
-    <div className="add-outfit-card">
-      <FontAwesomeIcon icon={faCirclePlus} size="lg" type="button" />
+    <div
+      className="add-outfit-card"
+      onClick={() => console.log('Add current product to outfit...')}
+      onKeyPress={() => console.log('Add current product to outfit...')}
+      role="button"
+      tabIndex={0}
+    >
+      <FontAwesomeIcon
+        className="add-outfit-plus"
+        icon={faCirclePlus}
+        size="xl"
+        type="button"
+      />
       <p>Add Outfit</p>
     </div>
   );
--- src/components/related_items_outfit/YourOutfit.jsx
+++ src/components/related_items_outfit/YourOutfit.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import AddOutfitCard from './AddOutfitCard';
+import Carousel from './Carousel';
+import ProductCard from './RIProductCard';
+import Compare from './CompareModal';
 
-export default function YourOutfit() {
+export default function YourOutfit({ curProd }) {
+  const [outfit, setOutfit] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleAdd = () => {
+    let newOutfit = outfit;
+    newOutfit.push(curProd);
+    setOutfit(newOutfit);
+  };
+
   return (
     <div>
       <h2>Your Outfit</h2>
@@ -12,8 +25,22 @@ export default function YourOutfit() {
         }}
       >
         {/* Conditional render if there are no products on Outfit */}
-        <AddOutfitCard />
+        <AddOutfitCard handleAdd={handleAdd} />
+        <Carousel>
+          <ProductCard setOpenModal={setOpenModal} />
+        </Carousel>
+        {openModal && (
+          <Compare
+            curProdID="40005"
+            compProdID="40006"
+            setOpenModal={setOpenModal}
+          />
+        )}
       </section>
     </div>
   );
 }
+
+YourOutfit.propTypes = {
+  curProdID: PropTypes.number.isRequired,
+};
