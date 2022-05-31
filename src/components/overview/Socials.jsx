52a9293888a73f5dde007aabdab67785275726b1
Andres Arango <ararango@comcast.net>
2022-05-31 10:35:08 -0700

    added color to social buttons on hover

--- src/components/overview/Socials.jsx
+++ src/components/overview/Socials.jsx
@@ -22,14 +22,24 @@ export default function Socials() {
         rel="noreferrer"
         href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fhackreactor&amp;src=sdkpreparse"
       >
-        <FontAwesomeIcon icon={faFacebookSquare} color="grey" size="2xl" />
+        <FontAwesomeIcon
+          icon={faFacebookSquare}
+          color="grey"
+          size="2xl"
+          className="fb-social"
+        />
       </a>
       <a
         target="_blank"
         rel="noreferrer"
         href="https://twitter.com/intent/tweet?hashtags=FEC&original_referer=https%3A%2F%2Fpublish.twitter.com%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&text=Team%20Verde%20really%20killed%20it%20%40HackReactor%20https%3A%2F%2Fgithub.com%2FRFP-Verde"
       >
-        <FontAwesomeIcon icon={faTwitterSquare} color="grey" size="2xl" />
+        <FontAwesomeIcon
+          icon={faTwitterSquare}
+          color="grey"
+          size="2xl"
+          className="twt-social"
+        />
       </a>
       <a
         target="_blank"
@@ -38,7 +48,12 @@ export default function Socials() {
         data-pin-do="buttonBookmark"
         data-pin-tall="true"
       >
-        <FontAwesomeIcon icon={faPinterestSquare} color="grey" size="2xl" />
+        <FontAwesomeIcon
+          icon={faPinterestSquare}
+          color="grey"
+          size="2xl"
+          className="pnt-social"
+        />
       </a>
     </section>
   );
