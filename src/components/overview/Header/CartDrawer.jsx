3aad1b41eab0ff5a984914ac7418f8711d98e716
Andres Arango <ararango@comcast.net>
2022-06-01 21:22:04 -0700

    semi-successfully implement exit animation for drawer

--- src/components/overview/Header/CartDrawer.jsx
+++ src/components/overview/Header/CartDrawer.jsx
@@ -7,54 +7,35 @@ import config from '../../../../config/config';
 import CartDrawerItem from './CartDrawerItem';
 import CartDrawerBtns from './CartDrawerBtns';
 
-export default function CartDrawer({ closeDrawer }) {
+export default function CartDrawer({ drawerClass, closeDrawer }) {
   const [cartItems, setCartItems] = useState([]);
   const [refreshCart, setRefreshCart] = useState(0);
+  const [closingOut, setClosingOut] = useState(false);
 
   const cartElements = cartItems.map((item) => (
     <CartDrawerItem skuId={item.sku_id} count={item.count} key={item.sku_id} />
   ));
 
-  useEffect(() => {
-    axios
-      .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/cart', {
-        headers: { Authorization: config.TOKEN },
-      })
-      .then(({ data }) => setCartItems(data))
-      .catch(() => setCartItems([]));
-  }, [refreshCart]);
+  // useEffect(() => {
+  //   axios
+  //     .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/cart', {
+  //       headers: { Authorization: config.TOKEN },
+  //     })
+  //     .then(({ data }) => setCartItems(data))
+  //     .catch(() => setCartItems([]));
+  // }, [refreshCart]);
 
   return (
-    <div
-      style={{
-        position: 'fixed',
-        width: '100vw',
-        height: '100vh',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: 100000,
-      }}
-    >
+    <div className="drawer--container">
       <div
-        style={{
-          position: 'fixed',
-          width: '100vw',
-          height: '100vh',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background: 'rgba(49, 49, 49, 0.8)',
-        }}
+        className={`drawer-window ${drawerClass}`}
         onClick={closeDrawer}
         onKeyDown={closeDrawer}
         role="button"
         tabIndex={0}
         aria-label="transparent div"
       />
-      <div className="header--cart-drawer">
+      <div className={`header--cart-drawer ${drawerClass}`}>
         <div>
           <h3 className="cart-drawer--header">Your Cart</h3>
           {cartElements}
@@ -74,5 +55,6 @@ export default function CartDrawer({ closeDrawer }) {
 }
 
 CartDrawer.propTypes = {
+  drawerClass: PropTypes.string.isRequired,
   closeDrawer: PropTypes.func.isRequired,
 };
--- src/components/overview/Header/HeaderCart.jsx
+++ src/components/overview/Header/HeaderCart.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import CartDrawer from './CartDrawer';
 
 export default function HeaderCart() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [showDrawer, setShowDrawer] = useState(false);
+  const [keepDrawer, setKeepDrawer] = useState(false);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  useEffect(() => {
+    let timeoutId;
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+    } else if (!showDrawer && !isFirstLoad) {
+      setKeepDrawer(true);
+      timeoutId = setTimeout(() => {
+        setKeepDrawer(false);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showDrawer]);
 
   const closeDrawer = () => {
-    setIsOpen(false);
+    setShowDrawer(false);
   };
 
   return (
@@ -17,9 +35,14 @@ export default function HeaderCart() {
         color="white"
         size="xl"
         type="button"
-        onClick={() => setIsOpen(true)}
+        onClick={() => setShowDrawer(true)}
       />
-      {isOpen && <CartDrawer closeDrawer={closeDrawer} />}
+      {(keepDrawer || showDrawer) && (
+        <CartDrawer
+          closeDrawer={closeDrawer}
+          drawerClass={showDrawer ? 'fade-in' : 'fade-out'}
+        />
+      )}
     </div>
   );
 }
