e640deccd49e564d7c128d06501802f6abb77012
Andres Arango <ararango@comcast.net>
2022-06-01 21:55:09 -0700

    animate drawer x icon

--- src/components/overview/Header/HeaderCart.jsx
+++ src/components/overview/Header/HeaderCart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import CartDrawer from './CartDrawer';
