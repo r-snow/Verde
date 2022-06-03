72f310be514907e10ab6ac226ed14b4caa3b41e9
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-06-02 17:33:59 -0700

    Replaced next/prev with arrows.

--- src/components/related_items_outfit/Carousel.jsx
+++ src/components/related_items_outfit/Carousel.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function Carousel({ children }) {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -32,49 +34,76 @@ export default function Carousel({ children }) {
 
   return (
     <div className="carousel">
-      <div
-        className="inner"
-        style={{
-          transform: `translateX(-${activeIndex * 260}px)`,
-          whiteSpace: 'nowrap',
-          transition: 'transform 0.5s',
-        }}
-        onMouseEnter={() => setPaused(true)}
-        onMouseLeave={() => setPaused(false)}
-      >
-        {React.Children.map(children, (child) => React.cloneElement(child))}
-      </div>
-      <div
-        className="indicators"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          margin: '5px',
-        }}
-      >
-        <button
-          type="button"
+      <FontAwesomeIcon
+        icon={faAngleLeft}
+        // className={currImgIdx !== 0 ? 'hover-pointer' : ''}
+        size="xl"
+        type="button"
+        // onClick={() => {
+        //   if (currImgIdx) {
+        //     decrementIdx();
+        //   }
+        // }}
+        // color={currImgIdx === 0 ? 'transparent' : 'black'}
+        style={{ marginLeft: '1em' }}
+      />
+      <div className="viewport">
+        <div
+          className="inner"
           style={{
+            transform: `translateX(-${activeIndex * 260}px)`,
+            whiteSpace: 'nowrap',
+            transition: 'transform 0.5s',
+          }}
+          onMouseEnter={() => setPaused(true)}
+          onMouseLeave={() => setPaused(false)}
+        >
+          {React.Children.map(children, (child) => React.cloneElement(child))}
+        </div>
+        {/* <div
+          className="indicators"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
             margin: '5px',
           }}
-          onClick={() => {
-            updateIndex(activeIndex - 1);
-          }}
         >
-          Prev
-        </button>
         <button
-          type="button"
-          style={{
-            margin: '5px',
-          }}
-          onClick={() => {
-            updateIndex(activeIndex + 1);
-          }}
-        >
-          Next
-        </button>
+        type="button"
+            style={{
+              margin: '5px',
+            }}
+            onClick={() => {
+              updateIndex(activeIndex - 1);
+            }}
+            >
+            Prev
+            </button>
+            <button
+            type="button"
+            style={{
+              margin: '5px',
+            }}
+            onClick={() => {
+              updateIndex(activeIndex + 1);
+            }}
+            >
+            Next
+            </button>
+          </div> */}
       </div>
+      <FontAwesomeIcon
+        icon={faAngleRight}
+        // className={currImgIdx !== photos.length - 1 ? 'hover-pointer' : ''}
+        size="xl"
+        type="button"
+        // onClick={() => {
+        //   if (currImgIdx !== photos.length - 1) {
+        //     incrementIdx();
+        //   }
+        // }}
+        // color={currImgIdx === photos.length - 1 ? 'transparent' : 'black'}
+      />
     </div>
   );
 }
